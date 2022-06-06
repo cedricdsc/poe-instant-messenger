8fce8cdd71b8b9bea66b3e3ef76d3e691745cb6a
cedricdsc <cedric.dsc@hotmail.de>
2022-06-06 19:41:09 +0200

    fix logfile tailing time by disabling follow option

--- src/main/LogWatcher/watch.ts
+++ src/main/LogWatcher/watch.ts
@@ -4,7 +4,6 @@ import Message from '../Message/Message';
 import Store from '../Store/ElectronStore';
 import Character from '../Character/Character';
 import { IpcEvent } from '../IpcEvent/IpcEvent';
-import { TradeStatus } from '../Character/CharacterStatus';
 
 const DEFAULT_PATH =
   'C:/Program Files (x86)/Grinding Gear Games/Path of Exile/logs/Client.txt';
@@ -19,24 +18,10 @@ function isWhisperFromUser(data: string) {
   return undefined;
 }
 
-function isTradeMessage(data: string) {
-  const startOfMessage = data.indexOf(': ') - 2;
-  const message = data.substring(startOfMessage);
-  const isTradeCancelled = message.match('] : Trade cancelled.');
-  const isTradeAccepted = message.match('] : Trade accepted.');
-  if (isTradeCancelled || isTradeAccepted) {
-    const tradeStatus = isTradeCancelled
-      ? TradeStatus.Idle
-      : TradeStatus.Accepted;
-    return { isTrade: true, tradeStatus };
-  }
-  return { isTrade: false, tradeStatus: undefined };
-}
-
 export default function startLogWatcher(cb: (event: IpcEvent) => void) {
   const storedPath = Store.get('settings.logPath');
   const path = typeof storedPath === 'string' ? storedPath : DEFAULT_PATH;
-  const tail = new Tail(path, { useWatchFile: true });
+  const tail = new Tail(path, { follow: false });
 
   tail.on('line', (data: string) => {
     let messageStore = Store.get('messageStore');
@@ -84,17 +69,6 @@ export default function startLogWatcher(cb: (event: IpcEvent) => void) {
           name: 'MAIN->OVERLAY::notify',
           payload: { from: message.username, message: message.text },
         });
-      }
-    } else {
-      const { isTrade, tradeStatus } = isTradeMessage(data);
-      if (isTrade && tradeStatus) {
-        messageStore = messageStore.map((entry) => {
-          if (entry.tradeStatus === TradeStatus.Initiated) {
-            entry.tradeStatus = tradeStatus;
-          }
-          return entry;
-        });
-        Store.set('messageStore', messageStore);
       }
     }
   });
