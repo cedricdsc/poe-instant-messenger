ff64072d419539610a95cf73592e783a83de840a
cedricdsc <cedric.dsc@hotmail.de>
2022-06-14 12:10:04 +0200

    add native notification sound support

---
+++ src/main/LogWatcher/playNotificationSound.ts
@@ -0,0 +1,25 @@
+import fs from 'fs';
+import wav from 'wav';
+import Speaker from 'speaker';
+import { getAssetPath } from '../util';
+
+let isSoundPlaying = false;
+
+export default function playNotificationSound() {
+  if (!isSoundPlaying) {
+    const reader = new wav.Reader();
+
+    reader.on('format', (format) => {
+      reader.pipe(new Speaker(format));
+    });
+
+    const file = fs.createReadStream(
+      getAssetPath('sounds/notification_sound_1.wav')
+    );
+    file.pipe(reader);
+    isSoundPlaying = true;
+    setTimeout(() => {
+      isSoundPlaying = false;
+    }, 2000);
+  }
+}
